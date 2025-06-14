2b2700b77ec0ceddf13bc5f8b04e5d8a6810dcc2
SHUBHRANIL CHOWDHURY <subhranilchowdhury27@gmail.com>
2025-06-14 18:06:14 +0530

    check

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,6 @@ const App = () => {
     <div>
       <div className="flex min-h-svh flex-col items-center justify-center">
         <Button>Click me</Button>
-        <>hello world</>
       </div>
     </div>
   );
